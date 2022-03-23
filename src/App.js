ad87ff348b71f60aa484befac31193059ab3c2c4
Jana Pascual <jacpascual@gmail.com>
2022-03-23 02:11:15 -0400

    Adds more styling:

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import VillagerList from './pages/VillagerList/VillagerList';
 function App() {
   return (
     <>
-      <h1>Sanity Check</h1>
-      <a href='/fossils'>To the fossils!</a>
-      <a href='/villagers'>To the villagers!</a>
+      <h1>Animal Crossing</h1>
+      <div className='links'>
+        <a href='/fossils'>To the fossils!</a>
+        <a href='/villagers'>To the villagers!</a>
+      </div>
       <Routes>
         <Route path='/fossils' element={<FossilList />} />
         <Route path='/villagers' element={<VillagerList />} />
